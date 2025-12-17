10e586215ab7ef4bcbb9787a1deb5e123bebf2e6
Ragul-Devops <ragul493@gmail.com>
2025-12-18 03:48:00 +0530

    Add Java app, JUnit test and SonarQube Maven config

---
+++ src/main/java/com/ragul/App.java
@@ -0,0 +1,11 @@
+package com.ragul;
+public class App {
+
+	    public static void main(String[] args) {
+		            System.out.println("Hi, welcome to Ragul's CI/CD Pipeline Project!");
+			        }//Method used for unit testing
+	    public static String getMessage() {
+		            return "Hi, welcome to Ragul's CI/CD Pipeline Project!";
+			        }
+}
+
