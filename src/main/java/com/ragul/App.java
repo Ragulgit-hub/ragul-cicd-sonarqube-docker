f28db3226e73bccbb076f7aa44dda46fc321e902
Ragul-Devops <ragul493@gmail.com>
2025-12-18 05:24:53 +0530

    Fix message to match unit test expectation

--- src/main/java/com/ragul/App.java
+++ src/main/java/com/ragul/App.java
@@ -1,11 +1,12 @@
 package com.ragul;
+
 public class App {
 
-	    public static void main(String[] args) {
-		            System.out.println("Hi, welcome to Ragul's CI/CD Pipeline Project!");
-			        }//Method used for unit testing
-	    public static String getMessage() {
-		            return "Hi, welcome to Ragul's CI/CD Pipeline Project!";
-			        }
-}
+    public String getMessage() {
+        return "Hi, welcome to the Ragul's CI/CD pipeline project!";
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Hi, welcome to the Ragul's CI/CD pipeline project!");
+    }
+}
